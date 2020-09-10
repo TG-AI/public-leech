24e56c91168abd6f8a52fb7369303e0eebb5d5d8
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2020-09-11 00:37:14 +0530

    Update sample_config.py

--- tobrot/sample_config.py
+++ tobrot/sample_config.py
@@ -34,7 +34,7 @@ class Config:
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "./DOWNLOADS")
     # Telegram maximum file upload size
     MAX_FILE_SIZE = int(get_config("MAX_FILE_SIZE", 50000000))
-    TG_MAX_FILE_SIZE = int(get_config("TG_MAX_FILE_SIZE", 2097151000))
+    TG_MAX_FILE_SIZE = int(get_config("TG_MAX_FILE_SIZE", 2097152000))
     FREE_USER_MAX_FILE_SIZE = int(
         get_config("FREE_USER_MAX_FILE_SIZE", 50000000)
     )
@@ -70,7 +70,7 @@ class Config:
     ))
     MAX_TG_SPLIT_FILE_SIZE = int(get_config(
         "MAX_TG_SPLIT_FILE_SIZE",
-        1900000000
+        2000000000
     ))
     # add config vars for the display progress
     FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "█")
